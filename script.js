78ab261aa4e701c154727b747ba1c66f709a0494
Camila Alvarez <cami.alvarezc94@gmail.com>
2022-11-15 12:38:56 -0300

    SegundaEntregaJS

--- script.js
+++ script.js
@@ -1,62 +1,49 @@
-/*VUELO
+const vuelos = [
+    { pais: "madrid", precio: 250000 },
+    { pais: "los angeles", precio: 350000 },
+    { pais: "new York", precio: 310000 },
+    { pais: "tailandia", precio: 410000 },
+    { pais: "australia", precio: 470000 },
+    { pais: "tokio", precio: 510000 },
+]
 
-Madrid: 
-1 cuota $210.000
-6 cuotas + 15% interes
-12 cuotas + 30% interes
-*/
+let elegirprecio = parseInt(prompt("Ingrese el precio máximo a pagar por su vuelo."))
 
-/*let madridprecio= 210000
-let destino= "Madrid"
-alert("Vuelo a" + " " + destino +" " + "ida y vuelta")
-let cuotas= parseInt(prompt("Ingrese cantidad de cuotas: 1, 6 o 12"))
+let vuelosEconomicos = vuelos.filter(vuelo => vuelo.precio <= elegirprecio)
 
-while (cuotas == 1 || cuotas == 6 || cuotas == 12){
-    
+let salida = ''
+for (const vueloEconomico of vuelosEconomicos) {
+    salida = salida + vueloEconomico.pais + " " + vueloEconomico.precio + "\n"
+}
+alert("Los vuelos disponibles son: " + "\n" + salida)
 
-    if(cuotas==1){
-        console.log("El precio final es $"+ madridprecio)
+
+
+
+
+/*let pais1= "madrid"
+let pais2= "los angeles"
+let pais3= "new york"
+let pais4= "tailandia"
+let pais5= "australia"
+let pais6= "tokio"
+
+let paisesconcatenados = pais1 + pais2 + pais3 + pais4 + pais5 + pais6*/
+
+
+
+
+    /*if (elegirprecio == economicos){
+        alert("Los vuelos mas economicos son " economicos
+        );
+        
+    } else(elegirprecio == "z"){
+        alert("Nuestros destinos son Madrid: $250.000, New York: $310.000, Los Angeles: $350.000, Tailandia: $410.000, Australia: $470.000, Tokio: $510.000");
     }
-    else if(cuotas==6){
-        console.log("El precio total es $"+ madridprecio*1.15)
-    }
-    else if(cuotas==12){
-        console.log("El precio final es $" + madridprecio*1.30)
-    }
+}
 
-    cuotas= parseInt(prompt("Ingrese cantidad de cuotas: 1, 6 o 12"))
-}*/
-
-
-let madridprecio= 210000
-let destino= "Madrid"
-alert("Vuelo a" + " " + destino +" " + "ida y vuelta")
-let cuotas= parseInt(prompt("Ingrese cantidad de cuotas: 1, 6 o 12"))
-
-do{
-    if(cuotas==1){
-        console.log("El precio final es $"+ madridprecio)
-    }
-    else if(cuotas==6){
-        console.log("El precio total es $"+ madridprecio*1.15)
-    }
-    else if(cuotas==12){
-        console.log("El precio final es $" + madridprecio*1.30)
-    }
-
-    cuotas= parseInt(prompt("Ingrese cantidad de cuotas: 1, 6 o 12"))
-
-}while(cuotas == 1 || cuotas == 6 || cuotas == 12)
-
-
-
-
-
-
-
-
-
-
+precioeconomico()
+alert("Los vuelos mas economicos son Madrid: $250.000, New York: $310.000 y Los Angeles: $350.000")*/
 
 
 
